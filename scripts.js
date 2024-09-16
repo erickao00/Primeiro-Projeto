0fc4250eb6fc3fd2982535017954e1f19be0eed5
Erick <erick.alice92@gmail.com>
2024-09-16 17:32:49 -0300

    primeiro commit - projeto Tesla

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Document = ir no html
+//queryselector = busca o que eu quiser que ele busque
+
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario() {
+    formulario.style.left = "50%"
+}
+
+function desaparecerformulario() {
+    formulario.style.left = "-50%"
+}
